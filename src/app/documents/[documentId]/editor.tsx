57f9f6a2aa55e9ea4837c221d1a2cfcb86f0bbb7
bakNa2t <ok2.lock@gmail.com>
2025-07-09 12:11:05 +0300

    Create tiptap Editor

---
+++ src/app/documents/[documentId]/editor.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEditor, EditorContent } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+export const Editor = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: "<p>Hello World! 🌎️</p>",
+  });
+
+  return (
+    <div className="size-full overflow-auto bg-[#FAFBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
+      <div className="flex justify-center min-w-max w-[816px] py-6 print:py-0 mx-auto print:w-full print:min-w-0">
+        <EditorContent editor={editor} />
+      </div>
+    </div>
+  );
+};
