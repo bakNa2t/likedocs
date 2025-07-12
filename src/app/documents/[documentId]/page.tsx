e47aa8ab5f9e59ebaf2fadf0e5c62321e8b3185d
bakNa2t <ok2.lock@gmail.com>
2025-07-12 12:43:14 +0300

    Add Toolbar

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,4 +1,5 @@
 import { Editor } from "./editor";
+import { Toolbar } from "./toolbar";
 
 interface DocumentIdProps {
   params: Promise<{ documentId: string }>;
@@ -11,6 +12,7 @@ const DocumentIdPage = async ({ params }: DocumentIdProps) => {
 
   return (
     <div className="min-h-screen bg-[#FAFBFD]">
+      <Toolbar />
       <Editor />
     </div>
   );
