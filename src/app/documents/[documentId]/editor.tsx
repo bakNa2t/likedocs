f65da04206417b3fcf40964c31d523dfb097d178
bakNa2t <ok2.lock@gmail.com>
2025-07-14 11:06:24 +0300

    Add Link extension to editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -14,6 +14,7 @@ import Underline from "@tiptap/extension-underline";
 import FontFamily from "@tiptap/extension-font-family";
 import TextStyle from "@tiptap/extension-text-style";
 import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link";
 
 import StarterKit from "@tiptap/starter-kit";
 
@@ -56,6 +57,11 @@ export const Editor = () => {
     },
     extensions: [
       StarterKit,
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+      }),
       Color,
       Highlight.configure({
         multicolor: true,
