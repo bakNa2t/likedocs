cc2b88f8b7439e588080770409bf4d9f32f8ce6e
bakNa2t <ok2.lock@gmail.com>
2025-07-17 11:11:13 +0300

    Add shadcn Menubar and expand File menu

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -1,5 +1,31 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import {
+  FileIcon,
+  FileJsonIcon,
+  FilePenIcon,
+  FilePlusIcon,
+  FileTextIcon,
+  GlobeIcon,
+  PrinterIcon,
+  TrashIcon,
+} from "lucide-react";
+import { BsFilePdf } from "react-icons/bs";
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 import { DocumentInput } from "./document-input";
 
 export const Navbar = () => {
@@ -12,6 +38,85 @@ export const Navbar = () => {
 
         <div className="flex flex-col">
           <DocumentInput />
+
+          <div className="flex flex-col">
+            <Menubar className="h-auto p-0 border-none shadow-none bg-transparent">
+              <MenubarMenu>
+                <MenubarTrigger className="h-auto text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted">
+                  File
+                </MenubarTrigger>
+                <MenubarContent className="print:hidden">
+                  <MenubarSub>
+                    <MenubarSubTrigger>
+                      <FileIcon className="size-4 mr-2" />
+                      Save
+                    </MenubarSubTrigger>
+
+                    <MenubarSubContent>
+                      <MenubarItem>
+                        <FileJsonIcon className="size-4 mr-2" />
+                        JSON
+                      </MenubarItem>
+                      <MenubarItem>
+                        <GlobeIcon className="size-4 mr-2" />
+                        HTML
+                      </MenubarItem>
+                      <MenubarItem>
+                        <BsFilePdf className="size-4 mr-2" />
+                        PDF
+                      </MenubarItem>
+                      <MenubarItem>
+                        <FileTextIcon className="size-4 mr-2" />
+                        Text
+                      </MenubarItem>
+                    </MenubarSubContent>
+                  </MenubarSub>
+
+                  <MenubarItem>
+                    <FilePlusIcon className="size-4 mr-2" />
+                    New document
+                  </MenubarItem>
+
+                  <MenubarSeparator />
+
+                  <MenubarItem>
+                    <FilePenIcon className="size-4 mr-2" />
+                    Rename
+                  </MenubarItem>
+
+                  <MenubarItem>
+                    <TrashIcon className="size-4 mr-2" />
+                    Remove
+                  </MenubarItem>
+
+                  <MenubarSeparator />
+
+                  <MenubarItem onClick={() => window.print()}>
+                    <PrinterIcon className="size-4 mr-2" />
+                    Print <MenubarShortcut>⌘P</MenubarShortcut>
+                  </MenubarItem>
+                </MenubarContent>
+              </MenubarMenu>
+
+              <MenubarMenu>
+                <MenubarTrigger className="h-auto text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted">
+                  Edit
+                </MenubarTrigger>
+              </MenubarMenu>
+
+              <MenubarMenu>
+                <MenubarTrigger className="h-auto text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted">
+                  Insert
+                </MenubarTrigger>
+              </MenubarMenu>
+
+              <MenubarMenu>
+                <MenubarTrigger className="h-auto text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted">
+                  Format
+                </MenubarTrigger>
+              </MenubarMenu>
+            </Menubar>
+          </div>
         </div>
       </div>
     </nav>
