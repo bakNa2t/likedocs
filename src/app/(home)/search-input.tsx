078404028fa5215952abf000b2c26fc87edcf94c
bakNa2t <ok2.lock@gmail.com>
2025-07-18 12:20:20 +0300

    Create navbar's SearchInput comp

---
+++ src/app/(home)/search-input.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { SearchIcon, XIcon } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export const SearchInput = () => {
+  const [value, setValue] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleClear = () => {
+    setValue("");
+    inputRef.current?.blur();
+  };
+
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <form className="relative max-w-[720px] w-full">
+        <Input
+          ref={inputRef}
+          value={value}
+          onChange={handleChange}
+          placeholder="Search"
+          className="w-full h-[48px] px-14 placeholder:text-neutral-700 md:text-base border-none focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px_1px_rgba(65,69,73,.15)] bg-[#f0f4f8] rounded-full focus-visible:ring-0 focus:bg-white"
+        />
+
+        <Button
+          type="submit"
+          variant="ghost"
+          size="icon"
+          className="absolute left-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full"
+        >
+          <SearchIcon />
+        </Button>
+
+        {value && (
+          <Button
+            type="button"
+            variant="ghost"
+            size="icon"
+            onClick={handleClear}
+            className="absolute right-3 top-1/2 -translate-y-1/2 [&_svg]:size-5 rounded-full"
+          >
+            <XIcon />
+          </Button>
+        )}
+      </form>
+    </div>
+  );
+};
