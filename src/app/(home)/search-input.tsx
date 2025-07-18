10df611c3e61928849eda9cd7c95195488753a89
bakNa2t <ok2.lock@gmail.com>
2025-07-18 13:28:12 +0300

    Add useSearchParams and handleSubmit to send form

--- src/app/(home)/search-input.tsx
+++ src/app/(home)/search-input.tsx
@@ -6,8 +6,11 @@ import { SearchIcon, XIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
+import { useSearchParams } from "@/hooks/use-search-param";
+
 export const SearchInput = () => {
-  const [value, setValue] = useState("");
+  const [search, setSearch] = useSearchParams("search");
+  const [value, setValue] = useState(search);
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -16,12 +19,19 @@ export const SearchInput = () => {
 
   const handleClear = () => {
     setValue("");
+    setSearch("");
+    inputRef.current?.blur();
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(value);
     inputRef.current?.blur();
   };
 
   return (
     <div className="flex flex-1 items-center justify-center">
-      <form className="relative max-w-[720px] w-full">
+      <form onSubmit={handleSubmit} className="relative max-w-[720px] w-full">
         <Input
           ref={inputRef}
           value={value}
