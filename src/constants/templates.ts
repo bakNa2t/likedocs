0aabea0dbe7782c24f4cae054d328e035d78e021
bakNa2t <ok2.lock@gmail.com>
2025-07-20 09:56:46 +0300

    Create blank templates const

---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: "blank",
+    label: "Blank document",
+    imageUrl: "templates/blank-document.svg",
+  },
+  {
+    id: "blank-letter",
+    label: "Letter",
+    imageUrl: "templates/blank-letter.svg",
+  },
+  {
+    id: "blank-cover-letter",
+    label: "Cover letter",
+    imageUrl: "templates/blank-cover-letter.svg",
+  },
+  {
+    id: "blank-resume",
+    label: "Resume",
+    imageUrl: "templates/blank-resume.svg",
+  },
+  {
+    id: "blank-bisuiness-letter",
+    label: "Business letter",
+    imageUrl: "templates/blank-business-letter.svg",
+  },
+  {
+    id: "blank-project-proposal",
+    label: "Project proposal",
+    imageUrl: "templates/blank-project-proposal.svg",
+  },
+  {
+    id: "blank-software-proposal",
+    label: "Software development proposal",
+    imageUrl: "templates/blank-software-proposal.svg",
+  },
+];
