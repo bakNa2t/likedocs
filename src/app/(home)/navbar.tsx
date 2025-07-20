ba328d7a36aa3e5170c8bda2f01ec9d01c03f97a
bakNa2t <ok2.lock@gmail.com>
2025-07-20 09:45:11 +0300

    Create page Navbar

---
+++ src/app/(home)/navbar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+import { SearchInput } from "./search-input";
+
+export const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between w-full h-full">
+      <div className="flex items-center gap-3 shrink-0 pr-6">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={36} height={36} />
+        </Link>
+
+        <h3 className="text-xl">Likedocs</h3>
+      </div>
+
+      <SearchInput />
+    </nav>
+  );
+};
