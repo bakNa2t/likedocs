ae7a1db524d5e15c1e2397bf29bcbd5ab6ff7ba9
bakNa2t <ok2.lock@gmail.com>
2025-07-22 04:43:21 +0300

    Add convex api useQuery to render documents

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,19 @@
-import Link from "next/link";
+"use client";
+
+import { useQuery } from "convex/react";
+
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
+
+import { api } from "../../../convex/_generated/api";
 
 const Home = () => {
+  const documents = useQuery(api.documents.get);
+
+  if (!documents) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="fixed top-0 left-0 right-0 h-16 z-10 bg-white p-4">
@@ -9,13 +21,10 @@ const Home = () => {
       </div>
 
       <div className="mt-16">
-        <h1 className="text-3xl font-bold">Welcome to Likedocs</h1>
-        Click{" "}
-        <Link href={"/documents/000"}>
-          <span className="text-blue-600 underline">&nbsp;here&nbsp;</span>
-        </Link>
-        to go to document id
+        <TemplatesGallery />
       </div>
+
+      {documents?.map(({ _id, title }) => <span key={_id}>{title}</span>)}
     </div>
   );
 };
