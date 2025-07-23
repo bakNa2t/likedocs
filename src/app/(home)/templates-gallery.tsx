a75d83f516cf4d1be54df866b954ecdd678234a1
bakNa2t <ok2.lock@gmail.com>
2025-07-23 22:43:27 +0300

    Add convex useMutation to create document

--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useMutation } from "convex/react";
+
 import {
   Carousel,
   CarouselContent,
@@ -10,9 +14,24 @@ import {
 
 import { cn } from "@/lib/utils";
 import { templates } from "@/constants/templates";
+import { api } from "../../../convex/_generated/api";
 
 export const TemplatesGallery = () => {
-  const isCreating = false;
+  const router = useRouter();
+  const create = useMutation(api.documents.create);
+
+  const [isCreating, setIsCreating] = useState(false);
+
+  const onTemplateClick = (title: string, initialContent: string) => {
+    setIsCreating(true);
+    create({ title, initialContent })
+      .then((documentId) => {
+        router.push(`/documents/${documentId}`);
+      })
+      .finally(() => {
+        setIsCreating(false);
+      });
+  };
 
   return (
     <div className="bg-[#f1f3f4]">
@@ -32,7 +51,6 @@ export const TemplatesGallery = () => {
                   )}
                 >
                   <button
-                    onClick={() => {}}
                     style={{
                       backgroundImage: `url(${template.imageUrl})`,
                       backgroundSize: "cover",
@@ -40,6 +58,7 @@ export const TemplatesGallery = () => {
                       backgroundRepeat: "no-repeat",
                     }}
                     className="flex flex-col items-center justify-center gap-y-4 bg-white hover:bg-blue-500 size-full border hover:border-blue-500 rounded-sm transition"
+                    onClick={() => onTemplateClick(template.label, "")}
                     disabled={isCreating}
                   />
                   <p className="text-sm font-medium truncate">
