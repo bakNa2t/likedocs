20192f8c2c7986570ddb986bb70a6f603699079c
bakNa2t <ok2.lock@gmail.com>
2025-07-28 15:19:05 +0300

    Add clerk's OrganizationSwitcher

--- src/app/(home)/navbar.tsx
+++ src/app/(home)/navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 import { SearchInput } from "./search-input";
 
@@ -17,7 +17,15 @@ export const Navbar = () => {
 
       <SearchInput />
 
-      <UserButton />
+      <div className="flex items-center gap-3 pl-6">
+        <OrganizationSwitcher
+          afterCreateOrganizationUrl="/"
+          afterLeaveOrganizationUrl="/"
+          afterSelectOrganizationUrl="/"
+          afterSelectPersonalUrl="/"
+        />
+        <UserButton />
+      </div>
     </nav>
   );
 };
