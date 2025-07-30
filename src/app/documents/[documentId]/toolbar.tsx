84d3b7a08b6e1e67d0c878c2c155a044d0fe599a
bakNa2t <ok2.lock@gmail.com>
2025-07-30 17:36:07 +0300

    Add editor's addPendingComment method to comment label

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -615,8 +615,8 @@ export const Toolbar = () => {
       {
         label: "Comment",
         icon: MessageSquarePlusIcon,
-        isActive: false,
-        onClick: () => console.log("Comment add"),
+        isActive: editor?.isActive("liveblocksCommentMark"),
+        onClick: () => editor?.chain().focus().addPendingComment().run(),
       },
       {
         label: "List Todo",
