2eb5816b1626073d4a61a52661dc247b66adf3f9
bakNa2t <ok2.lock@gmail.com>
2025-07-30 13:44:06 +0300

    Create LiveblocksProvider's room

---
+++ src/app/documents/[documentId]/room.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { ReactNode } from "react";
+import { useParams } from "next/navigation";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  const params = useParams();
+
+  return (
+    <LiveblocksProvider throttle={16} authEndpoint="/api/liveblocks-auth">
+      <RoomProvider id={params.documentId as string}>
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
