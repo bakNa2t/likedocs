05ed5985acd04adf39928d630983f19d63a866aa
bakNa2t <ok2.lock@gmail.com>
2025-08-01 12:02:46 +0300

    Create Inbox notifications

---
+++ src/app/documents/[documentId]/inbox.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { BellIcon } from "lucide-react";
+import { ClientSideSuspense } from "@liveblocks/react";
+import { useInboxNotifications } from "@liveblocks/react/suspense";
+import { InboxNotification, InboxNotificationList } from "@liveblocks/react-ui";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
+export const Inbox = () => {
+  return (
+    <ClientSideSuspense fallback={null}>
+      <InboxMenu />
+    </ClientSideSuspense>
+  );
+};
+
+const InboxMenu = () => {
+  const { inboxNotifications } = useInboxNotifications();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="relative">
+          <BellIcon className="size-5" />
+          {inboxNotifications.length > 0 && (
+            <span className="absolute -top-1 -right-1 flex items-center justify-center size-4 rounded-full text-xs text-white bg-sky-500">
+              {inboxNotifications.length}
+            </span>
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        {inboxNotifications.length > 0 ? (
+          <InboxNotificationList>
+            {inboxNotifications.map((inboxNotification) => (
+              <InboxNotification
+                key={inboxNotification.id}
+                inboxNotification={inboxNotification}
+              />
+            ))}
+          </InboxNotificationList>
+        ) : (
+          <div className="w-[400px] p-2 text-center text-sm text-muted-foreground">
+            No notifications
+          </div>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
