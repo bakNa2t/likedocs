8f7f137701390f3b834dc431a29f4f3777b06f53
bakNa2t <ok2.lock@gmail.com>
2025-08-04 17:00:38 +0300

    Create LoadingPage

---
+++ src/app/documents/[documentId]/loading.tsx
@@ -0,0 +1,7 @@
+import { FullScreenLoader } from "@/components/fullscreen-loader";
+
+const LoadingPage = () => {
+  return <FullScreenLoader label="Loading document..." />;
+};
+
+export default LoadingPage;
