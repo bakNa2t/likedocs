98227c022727a207bfaac708e610e71007ba37c9
bakNa2t <ok2.lock@gmail.com>
2025-08-22 14:10:33 +0300

    Add useLocale to toggle templates lang

--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useMutation } from "convex/react";
+import { useLocale } from "next-intl";
 import { toast } from "sonner";
 
 import {
@@ -19,6 +20,7 @@ import { api } from "../../../convex/_generated/api";
 
 export const TemplatesGallery = () => {
   const router = useRouter();
+  const locale = useLocale();
   const create = useMutation(api.documents.create);
 
   const [isCreating, setIsCreating] = useState(false);
@@ -42,36 +44,73 @@ export const TemplatesGallery = () => {
         <h3 className="font-medium">Start a new Document</h3>
         <Carousel>
           <CarouselContent className="-ml-4">
-            {templates.map((template) => (
-              <CarouselItem
-                key={template.id}
-                className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[14.285714%] pl-4"
-              >
-                <div
-                  className={cn(
-                    "flex flex-col gap-y-2.5 aspect-[3/4]",
-                    isCreating && "pointer-events-none opacity-50"
-                  )}
-                >
-                  <button
-                    style={{
-                      backgroundImage: `url(${template.imageUrl})`,
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
-                      backgroundRepeat: "no-repeat",
-                    }}
-                    className="flex flex-col items-center justify-center gap-y-4 bg-white hover:bg-blue-500 size-full border hover:border-blue-500 rounded-sm transition"
-                    onClick={() =>
-                      onTemplateClick(template.label, template.initialContent)
-                    }
-                    disabled={isCreating}
-                  />
-                  <p className="text-sm font-medium truncate">
-                    {template.label}
-                  </p>
-                </div>
-              </CarouselItem>
-            ))}
+            {locale === "en"
+              ? templates[0].map((template) => (
+                  <CarouselItem
+                    key={template.id}
+                    className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[14.285714%] pl-4"
+                  >
+                    <div
+                      className={cn(
+                        "flex flex-col gap-y-2.5 aspect-[3/4]",
+                        isCreating && "pointer-events-none opacity-50"
+                      )}
+                    >
+                      <button
+                        style={{
+                          backgroundImage: `url(${template.imageUrl})`,
+                          backgroundSize: "cover",
+                          backgroundPosition: "center",
+                          backgroundRepeat: "no-repeat",
+                        }}
+                        className="flex flex-col items-center justify-center gap-y-4 bg-white hover:bg-blue-500 size-full border hover:border-blue-500 rounded-sm transition"
+                        onClick={() =>
+                          onTemplateClick(
+                            template.label,
+                            template.initialContent
+                          )
+                        }
+                        disabled={isCreating}
+                      />
+                      <p className="text-sm font-medium truncate">
+                        {template.label}
+                      </p>
+                    </div>
+                  </CarouselItem>
+                ))
+              : templates[1].map((template) => (
+                  <CarouselItem
+                    key={template.id}
+                    className="basis-1/2 sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6 2xl:basis-[14.285714%] pl-4"
+                  >
+                    <div
+                      className={cn(
+                        "flex flex-col gap-y-2.5 aspect-[3/4]",
+                        isCreating && "pointer-events-none opacity-50"
+                      )}
+                    >
+                      <button
+                        style={{
+                          backgroundImage: `url(${template.imageUrl})`,
+                          backgroundSize: "cover",
+                          backgroundPosition: "center",
+                          backgroundRepeat: "no-repeat",
+                        }}
+                        className="flex flex-col items-center justify-center gap-y-4 bg-white hover:bg-blue-500 size-full border hover:border-blue-500 rounded-sm transition"
+                        onClick={() =>
+                          onTemplateClick(
+                            template.label,
+                            template.initialContent
+                          )
+                        }
+                        disabled={isCreating}
+                      />
+                      <p className="text-sm font-medium truncate">
+                        {template.label}
+                      </p>
+                    </div>
+                  </CarouselItem>
+                ))}
           </CarouselContent>
 
           <CarouselPrevious />
